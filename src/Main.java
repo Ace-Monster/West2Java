cc7cf43b8640f2f6a26fab2ff1cc5d812e106146
Ace-Monster <36086223+Ace-Monster@users.noreply.github.com>
2019-03-04 02:28:54 +0800

    Complete task 1(get www.qu.la)

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,11 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.*;
-import java.lang.annotation.Documented;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Scanner;
-
-class toRun implements Runnable{
-
-    public toRun(){
-
-    }
-
-    @Override
-    public void run(){
-
-    }
-}
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -54,80 +44,172 @@ public class Main {
         Class.forName("com.mysql.jdbc.Driver");
         conn = DriverManager.getConnection(sqlurl, Account, Password);
         st = conn.createStatement();
-        return;
     }
 
-    static void getBook(String booktab){
-        ResultSet rs;
-        try {
-            rs = st.executeQuery("select * from wait_que where book_id is null");
-            if(!rs.isBeforeFirst()) return;
-            while(rs.next()){
+    static public int getUrlID(String url){
+        Pattern pattern  = Pattern.compile("\\d+");
+        Matcher matcher  = pattern.matcher(url);
+        String sid = "Null";
+        while (matcher.find())
+            sid = matcher.group();
+        return Integer.parseInt(sid);
+    }
 
+    static void getBook(int bookid){
+        ResultSet rs;
+        while(true) {
+            try {
+                rs = st.executeQuery("select * from wait_que where url is not null");
+                if(!rs.isBeforeFirst()) //结果集为空，获取结束
+                    return;
+                rs.next();
+            } catch (SQLException ex) {
+                System.out.println("rs2");
+                return;
             }
+            ArrayList<toRun> q = new ArrayList<toRun>();
+            ArrayList<Thread> p = new ArrayList<Thread>();
+            for (int i = 1; i <= 8; i++) {
+                try {
+                    toRun tt = new toRun(rs, conn.createStatement(), bookid);
+                    Thread t = new Thread(tt);
+                    t.start();
+                    q.add(tt);
+                    p.add(t);
+                }catch (SQLException ex){
+                    System.out.println("建立进程:"+i);
+                    System.out.println(ex.getMessage());
+                }
+            }
+            boolean flag = false;
+            while (!flag) {
+                flag = true;
+                for (int i = 0; i < 8; i++) {
+                    flag = (flag & q.get(i).getFlag());
+                }
+            }
+        }
+    }
+
+    static void getUrl(Document doc, int bookid){
+        Elements title = doc.getElementById("list").getElementsByTag("dd");
+        for (int i = 0; i < title.size(); i++) {
+            String url = title.get(i).select("a").attr("abs:href");//章节链接
+            String name = title.get(i).text();
+            int id = getUrlID(url);
+            String sql = "insert into wait_que (id, url) value ("+id+", \""+url+"\") on duplicate key update id=values(id)";
+            try {
+                st.execute(sql);
+            }catch (SQLException ex){
+                System.out.println("inst url" + ex.getMessage());
+            }
+        }
+    }
+
+    static void updBook(int bookid, int isurl){
+        try {
+            st.execute("update wait_que set book_id=" + (bookid+1) + " is_url=" + 0 + "where book_id is not null");
         }catch (SQLException ex){
-            System.out.println("rs2");
-            return;
+            System.out.println("upd " + ex.getMessage());
         }
     }
 
     public static void main(String args[]) {
         String url = "https://www.qu.la/book/";
         Integer bookid = 0;
+        Integer isurl = 0;
         ResultSet rs;
         try{
             Con();
-            //获取当前书籍进度
-            rs = st.executeQuery("select * from wait_que where book_id is not null");
-            rs.next();
-            bookid = rs.getInt("book_id");
         }catch (ClassNotFoundException ex1){
             System.out.println("ClassError");
             return;
         }catch (SQLException ex2){
-            System.out.println("SQLError");
+            System.out.println(ex2.getMessage());
             return;
         }//连接数据库
 
         try {
-            while(true) {
-                //询问章节队列是否为空，否进入getBook方法
+            //获取当前书籍进度
+            st.execute("use Book");
+            rs = st.executeQuery("select * from wait_que where book_id is not null");
+            rs.next();
+            bookid = rs.getInt("book_id");
+            isurl = rs.getInt("is_url");
+        }catch (SQLException ex){
+            System.out.println(ex.getMessage());
+            return;
+        }
 
-                String turl = url + bookid.toString();
-                Document document = Jsoup.connect(turl).get();
-                //File a = new File("a.txt");
-                //System.setOut(new PrintStream(new BufferedOutputStream(new FileOutputStream(a)), true));
+        while(true) {
+            if(bookid > 167020)//不要问我这个数怎么来的，问就开花
+                break;
 
-                //图片url
-                String img = document.getElementById("fmimg").select("img").attr("abs:src");
-                System.out.println("img:" + img);
-
-                //主要信息
-                Element info = document.getElementById("maininfo");
-                String name = info.getElementById("info").getElementsByTag("h1").first().text();
-                System.out.println("name:" + name);
-                String author = info.getElementById("info").getElementsByTag("p").first().text();
-                author = author.replaceAll("作 者：", "");
-                System.out.println("author:" + author);
-
-                //简介
-                String intro = info.getElementById("intro").text();
-                System.out.println("intro:" + intro);
-
-                //章节名称获取
-                Elements title = document.getElementById("list").getElementsByTag("dd");
-                for (int i = 0; i < title.size(); i++) {
-                    System.out.println(title.get(i).select("a").attr("abs:href"));//章节链接
-                    System.out.println(title.get(i).text());
-                }
-
-                //调用getBook方法
-
-                //更新断点信
+            //询问url是否取完，是直接进入getBook方法
+            if(isurl == 1) {
+                getBook(bookid);
+                updBook(bookid, isurl);
+                bookid++;
+                isurl = 0;
             }
-        } catch (IOException ex){
-            System.out.println("解析出错/解析结束");
-            ex.printStackTrace();
+
+            String turl = url + bookid.toString();
+            Document document;
+            try {
+                document = Jsoup.connect(turl).timeout(100000).get();
+            }catch (IOException ex){
+                System.out.println(ex.getMessage());
+                break;
+            }
+            Element info = document.getElementById("maininfo");//主要信息
+            if(info==null){ //不存在主要信息标签，判断此ID对应书籍不存在
+                updBook(bookid, isurl);
+                bookid++;
+                isurl = 0;
+                continue;
+            }
+            //标题
+            String name = info.getElementById("info").getElementsByTag("h1").first().text();
+            //作者
+            String author = info.getElementById("info").getElementsByTag("p").first().text();
+            //图片url
+            String img = document.getElementById("fmimg").select("img").attr("abs:src");
+            author = author.replaceAll("作 者：", "");
+            //简介
+            String intro = info.getElementById("intro").text();
+
+            //插入信息
+            String sql = "insert into Bookshelf (id, img, `name`, author, intro) value (" + bookid + ", \"" + img +
+                    "\", \"" + name + "\", \"" + author + "\", \"" + intro+"\")" + "on duplicate key update id=values(id)";
+            try {
+                st.execute(sql);
+            } catch (SQLException ex){
+                System.out.println("inst info " + ex.getMessage());
+            }
+
+            try{
+                st.execute("create table bookid_"+bookid+" (id int not null, url text, `name` char(255), content text, " +
+                        "primary key (id))");
+            } catch (SQLException ex){
+                System.out.println(ex.getMessage());
+            }
+
+            //章节名称、url获取
+            getUrl(document, bookid);
+            try {
+                st.execute("update wait_que set is_url=1 where book_id is not null");
+            }catch (SQLException ex){
+                System.out.println("up2 "+ex.getMessage());
+            }
+            isurl = 1;
+
+            //调用getBook方法
+            getBook(bookid);
+
+            //更新断点信息
+            updBook(bookid, isurl);
+            bookid++;
+            isurl = 0;
         }
     }
 }
---
+++ src/toRun.java
@@ -0,0 +1,75 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class toRun implements Runnable{
+    ResultSet rs;
+    Statement st;
+    boolean flag;
+    int bookid;
+    public toRun(ResultSet a, Statement b, int c){
+        rs = a;st = b;bookid = c;
+    }
+
+    public boolean getFlag(){
+        return flag;
+    }
+
+    @Override
+    public void run(){
+        flag = false;
+        while(true) {
+            String url = null;
+            try{
+                url = rs.getString("url");//获取url
+            }catch (SQLException ex1){
+                System.out.println(ex1.getMessage());
+                break;//获取失败，从发生的可能上只有rs结果集指针为末尾
+            }
+            try {
+                rs.next();
+                int id = 0;
+                id = Main.getUrlID(url);
+                if (id != 0) {
+                    if(st.executeQuery("select * from bookid_"+bookid+" where id="+id).next()) {
+                        st.execute("delete from bookid_"+bookid+" where id="+id);
+                        continue;//判重
+                    }
+                    Document document = Jsoup.connect(url).timeout(100000).get();//获取html
+                    String name = document.getElementsByTag("h1").get(0).text();//章节名称
+
+                    Element content = document.getElementById("content");
+                    String str = content.toString();//获取章节文本
+                    {
+                        str = str.replaceAll("&nbsp;", "");
+                        str = str.replaceAll("<div id=\"content\">", "");
+                        str = str.replaceAll("　", " ");
+                        str = str.replaceAll("\\r|\\s|\\n|\\t*", "");
+                        str = str.replaceAll("<br>", "\n");
+                        str = str.replaceAll("<script>chaptererror\\(\\);</script></div>", "\n");
+                        str = str.replaceAll("\\n+", "\n");
+                    }//章节内容处理
+
+                    String sql = "insert into bookid_"+bookid+" (id, `name`, url, content) value " +
+                            "("+id+", \""+name+"\", \""+url+"\", \""+str+"\") " +
+                            "on duplicate key update id=values(id);";//判重后依旧可能发生时间片的切换
+                    st.execute(sql);
+                    st.execute("delete from wait_que where id="+id);
+                }
+            } catch (SQLException ex1) {
+                System.out.println(ex1.getMessage());
+            } catch (IOException ex2) {
+                System.out.println(ex2.getMessage());
+            }
+        }
+        flag = true;
+    }
+}
